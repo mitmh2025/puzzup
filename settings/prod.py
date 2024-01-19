defc2a4578584c052d3302f1263cb96ae311ea3f
Evan Broder <evan@ebroder.net>
2024-01-18 18:30:14 -0800

    Setup allowed hosts and sentry

--- settings/prod.py
+++ settings/prod.py
@@ -6,11 +6,10 @@ from settings.base import *  # pylint: disable=unused-wildcard-import,wildcard-i
 DEBUG = False
 SECURE_SSL_REDIRECT = True
 
-# FIXME: Where are you hosting PuzzUp?
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["puzzup.letswriteahunt.com"]
 
 sentry_sdk.init(
-    dsn="",
+    dsn="https://d3e924ca08ac12b462b61e5e17c73523@o4506595254599680.ingest.sentry.io/4506595264954368",
     integrations=[DjangoIntegration()],
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
