77f7302f662d09a29a2626967650f5cdaf51c258
Evan Broder <evan@ebroder.net>
2024-01-25 16:31:25 -0800

    Generate graphviz of puzzle statuses

---
+++ puzzle_editing/management/commands/generate_status_graph.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+
+from puzzle_editing import status
+
+
+BLOCKER_COLORS = {
+    status.EIC: "lemonchiffon",
+    status.EDITORS: "lightsalmon",
+    status.AUTHORS: "palegreen",
+    status.TESTSOLVERS: "white",
+    status.POSTPRODDERS: "orchid",
+    status.FACTCHECKERS: "lightskyblue",
+    status.NOBODY: "grey",
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        nodes = []
+        for s in status.ALL_STATUSES:
+            blocker = status.BLOCKERS_AND_TRANSITIONS.get(s["value"], [status.NOBODY])[
+                0
+            ]
+            nodes.append(
+                '  {} [label="{} {}",style=filled,color={}];'.format(
+                    s["value"],
+                    s["emoji"],
+                    s["display"],
+                    BLOCKER_COLORS.get(blocker, BLOCKER_COLORS[status.NOBODY]),
+                )
+            )
+
+        edges = []
+        for s, (_, transitions) in status.BLOCKERS_AND_TRANSITIONS.items():
+            for (t, label) in transitions:
+                edges.append('  {} -> {} [label="{}"];'.format(s, t, label))
+
+        print("digraph {")
+        print("\n".join(nodes))
+        print("\n".join(edges))
+        print("}")
