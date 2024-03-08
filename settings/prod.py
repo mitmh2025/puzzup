b7fff24529d99c87c5e48d1933a477644975c90d
Evan Broder <evan@ebroder.net>
2024-03-08 14:35:27 -0500

    Drop transaction sample rate

--- settings/prod.py
+++ settings/prod.py
@@ -15,10 +15,7 @@ ALLOWED_HOSTS = ["puzzup.letswriteahunt.com"]
 sentry_sdk.init(
     dsn="https://d3e924ca08ac12b462b61e5e17c73523@o4506595254599680.ingest.sentry.io/4506595264954368",
     integrations=[DjangoIntegration()],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+    traces_sample_rate=0.1,
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
