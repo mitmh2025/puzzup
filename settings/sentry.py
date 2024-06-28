0c8e7f34dd23d105e80a1d0c3e5a36bf583c86fd
Evan Broder <evan@ebroder.net>
2024-06-28 11:27:31 -0700

    Try harder to filter anonymous traces

--- settings/sentry.py
+++ settings/sentry.py
@@ -2,21 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from django.core.exceptions import DisallowedHost
-
 if TYPE_CHECKING:
     from sentry_sdk._types import Event, Hint
 
 
 def before_send_transaction(event: Event, hint: Hint) -> Event | None:
-    if "user" not in event or not event["user"]:
+    if "user" not in event or not event["user"] or not event["user"]["id"]:
         return None
-    return event
-
-
-def before_send(event: Event, hint: Hint) -> Event | None:
-    if "exc_info" in hint:
-        exc_type, exc_value, tb = hint["exc_info"]
-        if isinstance(exc_value, DisallowedHost):
-            return None
     return event
